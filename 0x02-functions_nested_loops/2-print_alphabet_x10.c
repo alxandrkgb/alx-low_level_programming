b1edfb8c30e0ea34e31f75982c9bca2a28b4a8cd
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-02-21 14:46:31 +0000

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,19 @@
 */
 void print_alphabet_x10(void)
 {
-int i = 0;
-char letter = 'a';
-
-while (i < 10)
-{
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
-letter = 'a';
-i++;
-}
+  char ch;
+  int i;
+  i = 0;
+  
+  while (i < 10)
+  {
+    ch = 'a';
+    while (ch <= 'z')
+    {
+      _putchar(ch);
+      ch++;
+    }
+    _putchar('\n');
+    i++;
+  }
 }
