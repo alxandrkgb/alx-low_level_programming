6bc773fcd9e7013a8e2c6442bc00550065225fad
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-02-23 06:17:07 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two multipliers
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
