ee3c090ce32b79acd926afe8dba1c368fd35027f
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-02-23 17:34:08 +0000

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,11 @@
+
+/**
+* Main.h - header file
+*
+* All function files called here
+*/
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +17,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+Give feedback
