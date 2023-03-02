58f4faa19dbbad6708b5be768109b172b8910498
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-02 07:34:58 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
+	int n;
 	int a[5];
 	int *p;
 
-	a[2] = 98;
-	p = &a[2];
+	a[2] = 1024;
+	p = &n;
 
 	/* add your line of code here */
-	printf("a[2] = %d\n", *(p));
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
 
 	return (0);
 }
