8493cb49bad6644fc8d8a79431e2a772fc4ae988
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-06 08:33:20 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,9 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0' && *s != c)
-{
-s++;
-}
-return (*s == c ? s : NULL);
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
