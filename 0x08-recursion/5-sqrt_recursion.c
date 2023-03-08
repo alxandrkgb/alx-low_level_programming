03f6e972a772ef800a7a775ef1abe74f4ef412e2
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-08 03:59:55 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * find_sqrt - returns the natural square root of a number
+ * @n: the number to find the square root of
+ * @i: the current guess for the square root
+ *
+ * Return: the natural square root of n, or -1
+ */
+int find_sqrt(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (find_sqrt(n, i + 1));
+	}
+}
+
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
  *
@@ -13,47 +37,12 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
+	else if (n == 0)
 	{
-		return (n);
+		return (0);
 	}
 	else
 	{
-		return (_sqrt_helper(n, 1, n));
-	}
-}
-
-
-/**
- * _sqrt_helper - Helper function for _sqrt_recursion
- * @n: The number to find the natural square root of
- * @min: The minimum possible square root.
- * @max: The maximum possible square root
- *
- * Return: The natural square root of the number,
- * or -1 if the number does not have a natural square root
- */
-int _sqrt_helper(int n, int min, int max)
-{
-	if (max < min)
-	{
-		return (-1);
-	}
-	else
-	{
-		int guess = (min + max) / 2;
-
-		if (guess * guess == n)
-		{
-			return (guess);
-		}
-		else if (guess * guess > n)
-		{
-			return (_sqrt_helper(n, min, guess - 1));
-		}
-		else
-		{
-			return (_sqrt_helper(n, guess + 1, max));
-		}
+		return (find_sqrt(n, 1));
 	}
 }
