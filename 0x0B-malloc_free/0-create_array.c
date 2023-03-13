51a0cff17f0a979a4a25f496daea501e20926a99
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-13 05:01:03 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,17 +12,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	unsigned int i;
+	char *s;
 
 	if (size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(char) * size);
-	if (array == NULL)
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
-		array[i] = c;
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
 
-	return (array);
+	return (s);
 }
