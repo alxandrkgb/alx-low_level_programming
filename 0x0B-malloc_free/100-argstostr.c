da2bd0690b39ef864b689ff5be108949d63313a6
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-13 07:55:00 -0700

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,34 +12,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, len = 0, pos = 0;
+	int i, j, len = 0;
 	char *str;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '0'; j++)
+		for (j = 0; av[i][j]; j++)
+		{
 			len++;
+		}
 		len++;
 	}
 
 	str = malloc(len * sizeof(char));
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
+	len = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
-			str[pos] = av[i][j];
-			pos++;
+			str[len++] = av[i][j];
 		}
-		str[pos] = '\n';
-		pos++;
+		str[len++] = '\n';
 	}
-	str[pos] = '\0';
+	str[len] = '\0';
 
 	return (str);
 }
