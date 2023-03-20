29899e89914f15b3b24227235003a0a90d72d03d
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-20 08:03:38 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,19 @@
  * @age: age of dog
  * @owner: owner of dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
