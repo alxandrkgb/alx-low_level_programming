1156ede37464f3212054c37533e1cc1d5840ccf9
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-03-30 08:23:43 -0700

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,9 @@
-section .data
-hello db 'Hello, Helberton', 0x0a
-fmt db '%s', 0x0a, 0x00
-
-section .text
-global main
-
+	global    main
+		extern    printf
 main:
-push rbp
-mov rbp, rsp
-
-; Call printf
-mov rdi, fmt
-mov rsi, hello
-mov rax, 0
-call printf
-
-; Exit
-mov eax, 0
-leave
-ret
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov         eax, 0
+	ret
+format: db 'Hello, Helberton\n',0
