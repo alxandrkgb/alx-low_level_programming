c91879de3819a6a280ffdda74de7fecd1d5ae1b5
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-05-18 05:17:37 -0700

    header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
+/* Structure for doubly linked list node */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -12,4 +23,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 int _putchar(char c);
 
-#endif
+#endif /* LISTS_H */
