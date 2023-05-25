7ad16a3da9244b036d86ecb8afe994076b897290
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-05-25 08:53:02 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD="alx-low_level_programming/0x18-dynamic_libraries/hack.so"
-./gm 9 8 10 24 75 9
+wget -P /tmp/ https://raw.github.com/Abel5173/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
