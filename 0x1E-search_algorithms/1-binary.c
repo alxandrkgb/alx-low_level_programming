199cb921df69647f92941a57aec00a5d449511f8
Gyekye Alexander <gyekyealexander1@gmail.com>
2023-12-12 17:40:25 -0600

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,29 +1,65 @@
 #include "search_algos.h"
 #include <stdio.h>
 
-int binary_search(int *array, size_t size, int value) {
-    size_t left = 0, right = size - 1, mid;
+/**
+ * print_array - prints an array of integers
+ * @array: pointer to the start of the array
+ * @size: size of the array (number of elements)
+ *
+ * Return: nothing
+ */
+void print_array(int *array, int size)
+{
+	int i;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL || size == 0)
+		return;
 
-    while (left <= right) {
-        size_t i;
+	printf("Searching in array: ");
 
-        printf("Searching in array: ");
-        for (i = left; i < right; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[i]);
+	for (i = 0; i < size; i++)
+	{
+		if (i == 0)
+			printf("%d", array[i]);
+		else
+			printf(", %d", array[i]);
+	}
 
-        mid = (left + right) / 2;
+	printf("\n");
+}
 
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+/**
+ * binary_search - find value in sorted array using binary search method
+ * @array: pointer to first element in array to be searched
+ * @size: size of the array (number of elements)
+ * @value: value to be searched for
+ *
+ * Return: index position of value or -1 if not found or array is null
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+	int mid;
 
-    return -1;
+	if (array == NULL || size == 0 || array[0] > value)
+		return (-1);
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		print_array(&array[low], (high - low) + 1);
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] > value)
+			high = mid - 1;
+
+		else
+			low = mid + 1;
+	}
+
+	return (-1);
 }
